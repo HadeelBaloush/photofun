edc7a9054735d38385d75ca88b8a2c7e00baff82
HadeelBaloush <hadeelbaloush@gmail.com>
2017-02-06 18:22:53 +0400

    done

--- server.js
+++ server.js
@@ -5,11 +5,6 @@ app.listen(port);
 app.use(express.static(__dirname + '/client'));
 
 
-// app.use(bodyParser.json());
-// app.use(express.static(__dirname + '/client'));
-
-// require('./middleware.js')(app, express);
-
 
 
 app.get('/test', function(req, res){
